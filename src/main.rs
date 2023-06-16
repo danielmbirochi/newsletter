0b93f8a672caf8b39d6133f74b7da7d253af59c3
Daniel <dani.mbirochi@gmail.com>
2023-06-16 07:42:05 -0300

    Bootstrap http webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/healthcheck", web::get().to(health_check))
+    })
+    .bind("localhost:3000")?
+    .run()
+    .await
 }
