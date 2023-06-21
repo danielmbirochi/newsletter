c7e1fb643214a77367d0a64a870b66409f41830a
Daniel <dani.mbirochi@gmail.com>
2023-06-21 08:37:57 -0300

    Instrument application - add request tracing support

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
 use uuid::Uuid;
 use chrono::Utc;
+use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct SubscriptionFormData {
@@ -13,6 +14,18 @@ pub async fn subscribe(
     form: web::Form<SubscriptionFormData>,
     conn: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding new subscriber.",
+        %request_id,
+        email = %form.email,
+        name = %form.name
+    );
+    let _request_span_guard = request_span.enter();
+
+    // We do not call `.enter` on query_span, `.instrument`
+    // takes care of it at the right moments in the query future lifetime.
+    let query_span = tracing::info_span!("Saving new subscriber to database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, name, email, subscribed_at)
@@ -24,11 +37,14 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(conn.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            eprintln!("Failed to execute query: {}", e);
+            tracing::error!("Failed to execute query : {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
