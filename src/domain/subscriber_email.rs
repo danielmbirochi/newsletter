5da06848128a49dbcc2f8c860c2c577e17752943
Daniel <dani.mbirochi@gmail.com>
2023-07-13 08:19:22 -0300

    Impl unit tests for validating domain constraints in SubscriberEmail

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -18,3 +18,33 @@ impl AsRef<str> for SubscriberEmail {
         &self.0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberEmail;
+    use claims::{assert_err, assert_ok};
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let empty = "".to_string();
+        assert_err!(SubscriberEmail::parse(empty));
+    }
+
+    #[test]
+    fn email_missing_symbol_is_rejected() {
+        let email_missing_symbol = "somerandomstring".to_string();
+        assert_err!(SubscriberEmail::parse(email_missing_symbol));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email_missing_subject = "@example.com".to_string();
+        assert_err!(SubscriberEmail::parse(email_missing_subject));
+    }
+
+    #[test]
+    fn email_with_subject_and_symbol_is_parsed_successfully() {
+        let email_with_subject_and_symbol = "some@email.com".to_string();
+        assert_ok!(SubscriberEmail::parse(email_with_subject_and_symbol));
+    }
+}
